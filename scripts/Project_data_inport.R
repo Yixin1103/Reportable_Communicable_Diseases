7f2b93b675823865b032115e117beb05fc6d783f
Yixin Guan <yixinguan@YixindeMacBook-Pro.local>
2021-01-29 02:15:30 -0500

    initial push

--- scripts/Project_data_inport.R
+++ scripts/Project_data_inport.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Get data from Open Data Toronto
-# Author: 
-#Date: 
+# Author: Yixin Guan
+#Date: Jan.27, 2021
 # Pre-requisites: None
 
 
@@ -9,11 +9,14 @@
 library(opendatatoronto)
 library(tidyverse)
 
+
+#### Get Data ####
 all_data <-
   opendatatoronto::search_packages("Annual Summary of Reportable Communicable Diseases") %>%
   opendatatoronto::list_package_resources() %>%
-  filter(name == "Disease Sex and Age groups 2018") %>%
+  filter(name == "Disease Sex and Age groups 2018") %>% #This is the dataset we are interested in among the Reportable Communicable Diseases database
   select (id) %>%
   opendatatoronto::get_resource()
 
-all_data
+#### Save Data ####
+write.csv(all_data,"inputs/data/raw_data.csv")
